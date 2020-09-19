e7f2e74bbf49d38402e7cca77337066101a6b38a
Muhammed Rashid <rashid@irisind.com>
2020-09-19 08:00:09 +0530

    here we go

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ require('./routes/api-routes')(app);
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
-  // app.get('*',(req,res)=>{
-  //   res.sendFile(path.join(__dirname,'client','build','index.html'))
-  // })
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'))
+  })
 }
 
 const PORT = process.env.PORT || 8082;
